3281ce079eac45608aa5ac0f8af33ecbcdfb29de
Swastik <sharmaspeedx29@gmail.com>
2025-11-11 15:39:57 +0530

    timer updared

--- src/App.jsx
+++ src/App.jsx
@@ -95,7 +95,7 @@ const App = () => {
   
       // Then remove the element after animation finishes
       setTimeout(() => setShowWarning(false), 500); // match fade-out duration
-    }, 10000);
+    }, 100000);
   
     return () => clearTimeout(timer);
   }, []);
